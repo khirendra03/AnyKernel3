3b157424d7641f6439820c68ea38df5aebe768a4
Khirendra <77091137+khirendra03@users.noreply.github.com>
2023-11-23 07:28:25 +0530

    AnyKernel3: adopt for X01BD

--- anykernel.sh
+++ anykernel.sh
@@ -1,10 +1,9 @@
-### AnyKernel3 Ramdisk Mod Script
 ## osm0sis @ xda-developers
 
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=taste kernel  by khirendra
+kernel.string=Luna kernel  by khirendra
 do.devicecheck=1
 do.modules=0
 do.systemless=1
@@ -15,7 +14,7 @@ device.name2=X01BDA
 device.name3=ASUS_X01BD
 device.name4=ASUS_X01BDA
 device.name5=Zenfone Max Pro M2 (X01BD)
-supported.versions=13.0
+supported.versions=14.0
 supported.patchlevels=
 supported.vendorpatchlevels=
 '; } # end properties
@@ -29,7 +28,7 @@ set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 } # end attributes
 
 # boot shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
@@ -37,30 +36,8 @@ patch_vbmeta_flag=auto;
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh;
 
-# Mount partitions as rw
-mount /system;
-mount /vendor;
-mount -o remount,rw /system;
-mount -o remount,rw /vendor;
-
 # boot install
 dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
-
-# init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
-
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "bootscript" init.tuna;
-
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-append_file fstab.tuna "usbdisk" fstab;
 
 write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
 ## end boot install
